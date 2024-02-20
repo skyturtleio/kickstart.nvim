998a0ebec81043c1b651fca70a0894d70a44bf70
Leo Mendoza Jr <dev@skyturtle.io>
2024-02-20 14:40:36 -0800

    add everblush colorscheme

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,8 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { 'Everblush/nvim', name = 'everblush' },
+}
 
 -- vim: ts=2 sts=2 sw=2 et

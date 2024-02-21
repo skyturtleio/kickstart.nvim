6b58bd2f7143b9afb6b9989704372f0c5ed2e2b3
skyturtle <dev@skyturtle.io>
2024-02-20 17:18:13 -0800

    remove tablines

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -163,11 +163,10 @@ require('lazy').setup({
         component_separators = '|',
         section_separators = '',
       },
-      tabline = {
-        lualine_a = { 'buffers' },
-        lualine_z = { 'tabs'}
-      }
-      ,
+      -- tabline = {
+      --   lualine_a = { 'buffers' },
+      --   lualine_z = { 'tabs'}
+      -- },
     },
   },
 

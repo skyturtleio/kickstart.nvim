2a81948df8ea2c2b55719e18f1980a38b811e5f2
Leo Mendoza Jr <dev@skyturtle.io>
2024-02-20 22:03:49 -0800

    revert back to <leader>f to use Telescope

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -49,7 +49,6 @@ end
 require('which-key').register {
   ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
   ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
   ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
   ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
   ['<leader>n'] = { name = '[N]eotree', _ = 'which_key_ignore' },

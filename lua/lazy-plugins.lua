ca06e87471b6642208a2550e94c1f0edaa328dc8
skyturtle <dev@skyturtle.io>
2024-02-20 21:24:31 -0800

    use relative line numbers, highlight on search, onedark deep

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -146,7 +146,7 @@ require('lazy').setup({
     config = function()
       require('onedark').setup {
         -- Set a style preset. 'dark' is default.
-        style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
+        style = 'deep', -- dark, darker, cool, deep, warm, warmer, light
       }
       require('onedark').load()
     end,

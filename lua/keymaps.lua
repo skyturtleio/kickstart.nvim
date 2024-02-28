cbc6c7bf38f5edc5d4320900c13e1227d781ef9b
Leo Mendoza Jr <dev@skyturtle.io>
2024-02-28 15:33:31 -0800

    capitalize Leader key

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -11,8 +11,8 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>de', vim.diagnostic.open_float, { desc = '[D]ocument [E]rrors in floating window' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+vim.keymap.set('n', '<Leader>de', vim.diagnostic.open_float, { desc = '[D]ocument [E]rrors in floating window' })
+vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -38,30 +38,30 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Neotree recommended keymaps
-vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle current reveal_force_cwd<cr>', { desc = '[E]xpanded explore files in Neotree' })
-vim.keymap.set('n', '<leader>x', '<cmd>Neotree toggle focus filesystem<CR>', { desc = 'E[X]plore files in Neotree' })
-vim.keymap.set('n', '<leader>b', '<cmd>Neotree toggle focus buffers right<CR>', { desc = 'Neotree show [B]uffers' })
-vim.keymap.set('n', '<leader>gn', '<cmd>Neotree float git_status<CR>', { desc = '[G]it [N]eotreej' })
+vim.keymap.set('n', '<Leader>e', '<cmd>Neotree toggle current reveal_force_cwd<cr>', { desc = '[E]xpanded explore files in Neotree' })
+vim.keymap.set('n', '<Leader>x', '<cmd>Neotree toggle focus filesystem<CR>', { desc = 'E[X]plore files in Neotree' })
+vim.keymap.set('n', '<Leader>b', '<cmd>Neotree toggle focus buffers right<CR>', { desc = 'Neotree show [B]uffers' })
+vim.keymap.set('n', '<Leader>gn', '<cmd>Neotree float git_status<CR>', { desc = '[G]it [N]eotreej' })
 
 -- Move lines up and down in Visual mode (ThePrimeagen video)
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line up in Visual mode' })
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line down in Visual mode' })
 
-vim.keymap.set('n', '<leader>df', '[mzt', { desc = '[D]ocument move current [F]uncion to top of screen' })
+vim.keymap.set('n', '<Leader>df', '[mzt', { desc = '[D]ocument move current [F]uncion to top of screen' })
 
 -- Keep cursor in place when paging up and down
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Keep cursor in place when paging up and down' })
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Keep cursor in place when paging up and down' })
 
 -- "Yank" to system clipboard
-vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]], { desc = '[Y]ank to system clipboard' })
-vim.keymap.set('n', '<leader>Y', [["+Y]], { desc = '[Y]ank to system clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<Leader>y', [["+y]], { desc = '[Y]ank to system clipboard' })
+vim.keymap.set('n', '<Leader>Y', [["+Y]], { desc = '[Y]ank to system clipboard' })
 
 -- Stay out of ex mode, don't press `Q`
 vim.keymap.set('n', 'Q', '<nop>', { desc = 'Stay out of ex mode' })
 
 -- Replace word you're on
-vim.keymap.set('n', '<leader>sw', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = '[S]ubstite [W]ord' })
+vim.keymap.set('n', '<Leader>sw', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = '[S]ubstite [W]ord' })
 
 -- [[ Basic Autocommands ]]
 --  See :help lua-guide-autocommands

2cd486e4e45f7f441e3218f30a42e2a8966de4dc
skyturtle <dev@skyturtle.io>
2024-02-29 12:21:53 -0800

    re-enable scrolloff

--- lua/options.lua
+++ lua/options.lua
@@ -53,7 +53,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
--- vim.opt.scrolloff = 10
+vim.opt.scrolloff = 10
 
 -- END OF ORIGINAL KICKSTART.NVIM
 

96d6b38d7be47af97f08dc8577670f7aa1ab3f82
skyturtle <dev@skyturtle.io>
2024-03-02 20:06:55 -0800

    move neotree keymaps to plugin file

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,14 +1,20 @@
-vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
 
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
   },
-  config = function ()
+  config = function()
     require('neo-tree').setup {}
+
+    -- Neotree recommended keymaps
+    vim.keymap.set('n', '<Leader>e', '<cmd>Neotree toggle current reveal_force_cwd<cr>', { desc = '[E]xpanded explore files in Neotree' })
+    vim.keymap.set('n', '<Leader>x', '<cmd>Neotree toggle focus filesystem<CR>', { desc = 'E[X]plore files in Neotree' })
+    vim.keymap.set('n', '<Leader>b', '<cmd>Neotree toggle focus buffers right<CR>', { desc = 'Neotree show [B]uffers' })
+    vim.keymap.set('n', '<Leader>gn', '<cmd>Neotree float git_status<CR>', { desc = '[G]it [N]eotreej' })
   end,
 }

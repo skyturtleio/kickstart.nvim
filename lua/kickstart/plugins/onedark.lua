28ad38f91fb40fbf0c32df16507775c1e946f708
Leo Mendoza Jr <dev@skyturtle.io>
2024-03-05 17:20:56 -0800

    switcht to tokyonight colorscheme

--- lua/kickstart/plugins/onedark.lua
+++ lua/kickstart/plugins/onedark.lua
@@ -1,14 +1,14 @@
 return {
   -- Theme inspired by Atom
   'navarasu/onedark.nvim',
-  priority = 1000,
-  lazy = false,
-  config = function()
-    require('onedark').setup {
-      -- Set a style preset. 'dark' is default.
-      style = 'deep', -- dark, darker, cool, deep, warm, warmer, light
-    }
-    require('onedark').load()
-  end,
+  -- priority = 1000,
+  -- lazy = false,
+  -- config = function()
+  --   require('onedark').setup {
+  --     -- Set a style preset. 'dark' is default.
+  --     style = 'deep', -- dark, darker, cool, deep, warm, warmer, light
+  --   }
+  --   require('onedark').load()
+  -- end,
 }
 -- vim: ts=2 sts=2 sw=2 et
